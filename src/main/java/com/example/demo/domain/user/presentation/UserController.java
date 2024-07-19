76f8ef72ca984364fe5954b2eca1f7b0753c67aa
yongbin1 <rhdydqls1024@naver.com>
2024-07-19 23:25:04 +0900

    Add :: User Basic Logic Development #4

--- src/main/java/com/example/demo/domain/user/presentation/UserController.java
+++ src/main/java/com/example/demo/domain/user/presentation/UserController.java
@@ -1,6 +1,11 @@
 package com.example.demo.domain.user.presentation;
 
+import com.example.demo.domain.user.presentation.dto.response.UserResponse;
+import com.example.demo.domain.user.service.FindUserInfoService;
+import com.example.demo.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final FindUserInfoService findUserInfoService;
+
+    @GetMapping("/info")
+    public BaseResponse<UserResponse> findUserInfo() {
+        UserResponse response = findUserInfoService.execute();
+        return new BaseResponse<>(HttpStatus.OK.value(), response);
+    }
+
 }
--- src/main/java/com/example/demo/domain/user/service/FindUserInfoService.java
+++ src/main/java/com/example/demo/domain/user/service/FindUserInfoService.java
@@ -1,2 +1,23 @@
-package com.example.demo.domain.user.service;public class FindUserInfoService {
+package com.example.demo.domain.user.service;
+
+import com.example.demo.domain.user.entity.User;
+import com.example.demo.domain.user.facade.UserFacade;
+import com.example.demo.domain.user.presentation.dto.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FindUserInfoService {
+
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public UserResponse execute() {
+        User user = userFacade.getCurrentLoginUser();
+
+        return UserResponse.of(user);
+    }
+
 }
